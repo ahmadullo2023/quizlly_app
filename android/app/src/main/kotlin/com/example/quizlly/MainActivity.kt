5b5ef07890368a9eb0432122234326ac7b58994b
ahmadullo2023 <ahmadullo2023@gmail.com>
2023-11-21 09:03:11 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/quizlly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizlly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/cupertino.dart';
+
+import 'src/common/widgets/app.dart';
+
+void main() => runApp(const MyApp());
